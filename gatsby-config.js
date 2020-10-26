5772ba82e2cc8a0b55947e11038b01039295a2fb
Maya Estalilla <mayaestalilla@gmail.com>
2020-10-26 00:34:24 -0400

    Basic project settings

--- gatsby-config.js
+++ gatsby-config.js
@@ -7,9 +7,9 @@ dotenv.config({
 
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `My Vinyl Collection`,
+    description: `A personal project used to demonstrate how to build an e-commerce site on the JAMStack.`,
+    author: `mayaestalilla@gmail.com`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
